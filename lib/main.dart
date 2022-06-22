342bf58251235beb113b23462c76ae87a0801b0f
marcelookasaki <marcelo.okasaki@gmail.com>
2022-06-22 09:47:17 -0300

    Inicial

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    print('Pergunta respondida!');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua banda favorita?',
+      'Qual é a sua música favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 2')),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 3')),
+          ],
+        ),
+      ),
+    );
+  }
+}
